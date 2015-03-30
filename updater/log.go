9546d1a796804b222f92abed5656a7f2c23c4619
capitancambio <capitan.cambio@gmail.com>
2015-03-30 11:03:07 +0100

    Modifications for logs

--- updater/log.go
+++ updater/log.go
@@ -1,8 +1,16 @@
 package updater
 
+import (
+	"fmt"
+	"io"
+	"os"
+)
+
+var Output io.Writer = os.Stdout
+
 func Info(format string, vals ...interface{}) {
+	fmt.Fprintf(Output, fmt.Sprintf("[INFO] %s\n", format), vals...)
 }
 func Error(format string, vals ...interface{}) {
-}
-func Panic(format string, vals ...interface{}) {
+	fmt.Fprintf(Output, fmt.Sprintf("[ERROR] %s\n", format), vals...)
 }
